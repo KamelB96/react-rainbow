feee6297f8d81cbabf1ed60134f300127ca59fa7
kamel Barakat <kamelbarakat96@yahoo.com>
2022-12-19 01:34:22 -0500

    using react to create a stunning rainbow

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+        let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+<div className="App">
+    {colors.map((color, i) => 
+        <ColorBlock key= color={color} />
+    )}
+</div>
+
+    )
+}
+
+
+
+export default App;
